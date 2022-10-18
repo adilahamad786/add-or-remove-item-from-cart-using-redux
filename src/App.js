259e970ebc340b88fa9c4f47a89ccd4213c93f1c
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-18 14:47:31 +0530

    Create our own action-creator-function-Thunk for managing async-task(http-fetch-data-request) using redux.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Notification from "./components/UI/Notification";
 
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -17,51 +17,12 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const sendCartData = async () => {
-
-      dispatch(
-        uiActions.showNotification({
-          status: "Pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-
-      const response = await fetch(
-        "https://add-items-in-cart-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending data is failed!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "Success",
-          title: "Success!",
-          message: "Sent cart data sucessfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart))
 
   }, [cart, dispatch]);
 
